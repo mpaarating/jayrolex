2330fcd083297d3357450f627c4dad0b9b148829
Alex Lewis <alexmichael.lewis@gmail.com>
2014-11-24 23:44:20 -0500

    refactored reviews page

--- reviews.php
+++ reviews.php
@@ -5,27 +5,13 @@ $page_title = "Jayrolex: Reviews";
 require_once ('includes/header.php');
 require_once ('includes/database.php');
 
-//select statement
-$query_str = "select * from reviews r join movies m where m.movie_id=r.review_movie_id";
-$movie_str = "select r.*, m.movie_name from reviews r join movies m on m.movie_id=r.review_movie_id";
+$movie_str = "select r.*, m.movie_name, m.movie_id from reviews r join movies m on m.movie_id=r.review_movie_id";
 
-//execut the query
-$result = $conn->query($query_str);
-$movie_result = $conn->query($movie_str);
+$resultset = $conn->query($movie_str);
 
-//Handle selection errors
-if (!$movie_result || !$movie_str) {
-	$errno = $conn->errno;
-	$errmsg = $conn->error;
-	echo "Selection failed with: ($errno) $errmsg<br/>\n";
-	$conn->close();
-	exit;
-}else {
-	$result_row = $result->fetch_assoc();
-	$movie_result_row = $movie_result->fetch_assoc();
+if ($resultset) {
 	?>
 	<div class="container wrapper">
-
 		<ul class="breadcrumb">
 			<li><a href="index.php">Home</a></li>
 			<li class="active">Reviews</li>
@@ -33,42 +19,35 @@ if (!$movie_result || !$movie_str) {
 
 		<h1 class="text-center">Reviews</h1>
 
-
-
-		<?php
-		//insert a row into the table for each row of data
-		while (($result_row = $result->fetch_assoc()) !== NULL && ($movie_result_row = $movie_result->fetch_assoc()) != NULL ) {
-			?>
+		<?php while ($row = $resultset->fetch_assoc()): ?>
 			<div class="row movie-list">
-
 				<div class="col-xs-12">
 					<div class="panel panel-default">
 						<div class="panel-heading">
 							<h3 class="panel-title">
-								<?php
-								echo "<a href='moviedetails.php?id=" . $result_row['review_movie_id'] . "'>", $movie_result_row['movie_name'], "</a>";
-								?>
+								<a href="moviedetailsphp?id=<?= $row['movie_id'] ?>"><?= $row['movie_name'] ?></a>
 							</h3>
 						</div>
 						<div class="panel-body">
-							<h4>Rating:
-								<?php
-								echo $result_row['review_rating'];
-								?>
+							<h4>
+								<?= $row['review_rating'] ?>
 							</h4>
-							<p class="lead"><?php echo $result_row['review_content'] ?></p>
+							<p class="lead"><?= $row['review_content'] ?></p>
 						</div>
 					</div>
 				</div>
 			</div>
-		<?php } ?>
+		<?php endwhile; ?>
 	</div>
-	<?php
-	// clean up resultsets when we're done with them!
-	$result->close();
+<?php
+} else {
+	?>
+	<div class="container wrapper">
+		<p>Selection failed with (<?= $conn->errno ?>) <?= $conn-error ?></p>
+	</div>
+<?php
 }
 
-// close the connection.
 $conn->close();
 
 include ('includes/footer.php');

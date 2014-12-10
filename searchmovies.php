d01a3fed1761e0a533aac2f08ac6059afaec5c07
M1llerT1me <millerross60@yahoo.com>
2014-12-09 19:19:19 -0500

    Movies.php has been changed and Searchmovies.php has been created.
    The form on movies.php now has an action and method.

--- searchmovies.php
+++ searchmovies.php
@@ -1,37 +1,39 @@
 <?php
 
-	$page_title = "Jayrolex: Movies";
-	
-	require_once ('includes/header.php');
-	require_once ('includes/database.php');
-	
-	//select statement
-	$query_str = "SELECT * FROM $tblMovies";
-	
-	//execut the query
-	$result = $conn->query($query_str);
-	
-	//Handle selection errors
-	if (!$result) {
-	    $errno = $conn->errno;
-	    $errmsg = $conn->error;
-	    echo "Selection failed with: ($errno) $errmsg<br/>\n";
-	    $conn->close();
-	    exit;
-	}else { //display results in a table
-	    ?>
-	<div class="container wrapper">
+$page_title = "Jayrolex: Movies";
 
+require_once ('includes/header.php');
+require_once('includes/database.php');
+
+$name = $_GET['movie'];
+
+//select statement
+$query_str = "SELECT * FROM $tblMovies WHERE movie_name LIKE '%" .$name. "%'";
+
+//execut the query
+$result = $conn->query($query_str);
+
+//Handle selection errors
+if (!$result) {
+    $errno = $conn->errno;
+    $errmsg = $conn->error;
+    echo "Selection failed with: ($errno) $errmsg<br/>\n";
+    $conn->close();
+    exit;
+} else { //display results in a table
+    ?>
+	<div class="container wrapper">
+		
 		<ul class="breadcrumb">
 			<li><a href="index.php">Home</a></li>
 			<li class="active">Movies</li>
 		</ul>
-
-		<h1 class="text-center">Movies</h1>
-
-		<div class="row">
+		
+	    <h1 class="text-center">Movies</h1>
+	    
+	    <div class="row">
 			<div class="col-xs-4 col-xs-offset-8">
-				<form action="searchmovies.php" method="get" class="form-inline search-form" role="form">
+				<form action="<?=$_SERVER['PHP_SELF']?>" class="form-inline search-form" method="get" role="form">
 					<div class="input-group">
 						<label class="sr-only" for="movieSearch">Search Movies</label>
 						<div class="input-group-addon"><i class="fa fa-search fa-lg"></i></div>
@@ -41,16 +43,19 @@
 				</form>
 			</div>
 		</div>
-
-
-		<div class="movie-list">
-		<?php
+    
+    <?php 
+    	$num_rows = mysqli_num_rows($result);
+    	if ($num_rows == 0) {
+    		echo "Your search of <strong>$name</strong> is incorrect. Type in a different title to search.";
+    	} else {
+        //insert a row into the table for each row of data
 		$i = 0;
 		while ( $result_row = $result->fetch_assoc() ) :
 			$i++;
 			if ($i == 1) :
-			?>
-		<div class="row">
+	?>
+			<div class="row">
 			<?php endif; ?>
 				<div class="col-xs-4">
 					<div class="thumbnail">
@@ -74,6 +79,7 @@
 		</div>
 	</div>
   <?php
+		}
 		// clean up resultsets when we're done with them!
 	    $result->close();
 	}
@@ -82,4 +88,3 @@
 	$conn->close();
 	
 	include ('includes/footer.php');
-?>

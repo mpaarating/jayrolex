fe3db27c57c6aedac09426ca2ae895697c2571f0
Alex Lewis <alexmichael.lewis@gmail.com>
2014-12-11 18:15:12 -0500

    changes nav

--- includes/header.php
+++ includes/header.php
@@ -65,43 +65,67 @@ if (isset($_SESSION['id'])) {
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-clock-o fa-lg"></i> JAYROLEX <i class="fa fa-caret-down"></i></a>
-			<ul class="dropdown-menu" role="menu">
-				<li><a href="index.php">HOME</a></li>
-				<li><a href="registration.php">REGISTER</a></li>
-				<li><a href="movies.php">MOVIES</a></li>
-				<li><a href="reviews.php">REVIEWS</a></li>
-			</ul>
+			<a href="index.php" class=" navbar-brand"><i class="fa fa-clock-o fa-lg"></i> JAYROLEX</a>
 		</div>
-		<div id="navbar" class="navbar-collapse collapse">
-			<?php
+		<div class="navbar-right">
+			<div id="navbar" class="navbar-collapse collapse">
+				<?php
 				if ($role == 1) : ?>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="addtoaccount.php">Welcome, <?php echo $name; ?>!</a></li>
-					<li><a href="addmovie.php">Add Movie</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			<?php
+					<ul class="nav navbar-nav">
+						<li><a href="addtoaccount.php" class="text-capitalize">Welcome, <?php echo $name; ?>!</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false"> Navigation <i class="fa fa-caret-down"></i></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="registration.php">REGISTER</a></li>
+								<li><a href="movies.php">MOVIES</a></li>
+								<li><a href="reviews.php">REVIEWS</a></li>
+								<li><a href="addmovie.php">ADD MOVIE</a></li>
+								<li><a href="logout.php">LOGOUT</a></li>
+							</ul>
+						</li>
+					</ul>
+				<?php
 				endif;
 				if ($role == 2) : ?>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="addtoaccount.php">Welcome, <?php print_r($name); ?>!</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			<?php
+					<ul class="nav navbar-nav">
+						<li><a href="addtoaccount.php" class="text-capitalize">Welcome, <?php print_r($name); ?>!</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false"> Navigation <i class="fa fa-caret-down"></i></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="registration.php">REGISTER</a></li>
+								<li><a href="movies.php">MOVIES</a></li>
+								<li><a href="reviews.php">REVIEWS</a></li>
+								<li><a href="logout.php">LOGOUT</a></li>
+							</ul>
+						</li>
+					</ul>
+				<?php
 				endif;
 				if (empty($login)) : ?>
-
-			<form class="navbar-form navbar-right" role="form" action="login.php" method="post">
-				<div class="form-group">
-					<input type="text" class="form-control" name="username" placeholder="Username" required>
-				</div>
-				<div class="form-group">
-					<input type="password" class="form-control" name="password" placeholder="Password" required>
-				</div>
-				<button type="submit" class="btn btn-success">SIGN IN</button>
-			</form>
-			<?php endif; ?>
-		</div><!--/.navbar-collapse -->
+					<form class="navbar-form navbar-right" role="form" action="login.php" method="post">
+						<div class="form-group">
+							<input type="text" class="form-control" name="username" placeholder="Username" required>
+						</div>
+						<div class="form-group">
+							<input type="password" class="form-control" name="password" placeholder="Password" required>
+						</div>
+						<button type="submit" class="btn btn-success">SIGN IN</button>
+					</form>
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false"> Navigation <i class="fa fa-caret-down"></i></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="registration.php">REGISTER</a></li>
+								<li><a href="movies.php">MOVIES</a></li>
+								<li><a href="reviews.php">REVIEWS</a></li>
+							</ul>
+						</li>
+					</ul>
+				<?php endif; ?>
+			</div>
+		</div><!--/.navbar-right -->
 	</div>
 </nav>
